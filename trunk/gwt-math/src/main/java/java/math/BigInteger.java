a54bd9150290cad7266027e40e19f62e76426c52
marco.gervasoni <marco.gervasoni@e5a20a11-9342-0410-9350-2f45a7d0be4e>
2008-02-08 22:41:07 +0000

    aggiunte chiamate RPC (da sistemare il package con i custmSerializer)

--- trunk/gwt-math/src/main/java/java/math/BigInteger.java
+++ trunk/gwt-math/src/main/java/java/math/BigInteger.java
@@ -4,52 +4,52 @@ import java.io.Serializable;
 
 
 
-public class BigInteger extends Number implements Cloneable,Serializable {
+public class BigInteger implements Cloneable,Serializable {
 	/*
 	 * MIEI CAMPI
 	 */
-	public BigDecimal internalBigInteger = null;
-	
-	private static final long serialVersionUID = -8287574255936472291L;
+	private transient BigDecimal internalBigInteger = null;
 
-	public static final BigInteger ZERO = new BigInteger("0");
+	private transient static final long serialVersionUID = -8287574255936472291L;
 
-	public static final BigInteger ONE = new BigInteger("1");
+	public transient static final BigInteger ZERO = new BigInteger("0");
 
-	
+	public transient static final BigInteger ONE = new BigInteger("1");
+
+
 	//*******************************************
-	
+
 	// Constructors
 	public BigInteger() {
 		this("0");
 	}
-	
-	public BigInteger(byte[] val) {
-		throw new IllegalArgumentException("Constructor BigInteger(byte[] val) not implemented");
-	}
 
-	public BigInteger(int signum, byte[] magnitude) {
-		throw new IllegalArgumentException("Constructor BigInteger(int signum, byte[] magnitude) not implemented");
-	}
-
-	public BigInteger(String val, int radix) {
-		throw new IllegalArgumentException("Constructor BigInteger(String val, int radix) not implemented");
-	}
+//	public BigInteger(byte[] val) {
+//		throw new IllegalArgumentException("Constructor BigInteger(byte[] val) not implemented");
+//	}
+//
+//	public BigInteger(int signum, byte[] magnitude) {
+//		throw new IllegalArgumentException("Constructor BigInteger(int signum, byte[] magnitude) not implemented");
+//	}
+//
+//	public BigInteger(String val, int radix) {
+//		throw new IllegalArgumentException("Constructor BigInteger(String val, int radix) not implemented");
+//	}
 
 	public BigInteger(String val) {
 		internalBigInteger = new BigDecimal(val);
 		internalBigInteger.setScale(0);
 	}
-	
+
 	private BigInteger convert(BigDecimal val) {
 		BigDecimal b = val.setScale(0);
 		return new BigInteger(b.toString());
 	}
 
-	public static BigInteger valueOf(long val) {
-		throw new IllegalArgumentException("Constructor not implemented");
-	}
-	
+//	public static BigInteger valueOf(long val) {
+//		throw new IllegalArgumentException("Constructor not implemented");
+//	}
+
 	// Arithmetic Operations
 
 	public BigInteger add(BigInteger val) {
@@ -68,41 +68,41 @@ public class BigInteger extends Number implements Cloneable,Serializable {
 	}
 
 	public BigInteger divide(BigInteger val) {
-		
+
 		BigDecimal val2 = internalBigInteger.divide(val.internalBigInteger,0,BigDecimal.ROUND_DOWN);
 		return convert(val2);
 	}
 
 	public BigInteger[] divideAndRemainder(BigInteger val) {
 		BigInteger[] result = new BigInteger[2];
-		
+
 		BigDecimal val2 = internalBigInteger.divide(val.internalBigInteger,0,BigDecimal.ROUND_DOWN);
-		
+
 		final BigInteger divide = convert(val2);
 		result[0] =divide;
-		
+
 		final BigInteger multiply = divide.multiply(val);
-		
-		final InternalBigDecimal subtract = internalBigInteger.internalBigDecimal.subtract(multiply.internalBigInteger.internalBigDecimal);
+
+		final InternalBigDecimal subtract = internalBigInteger.getInternalBigDecimal().subtract(multiply.internalBigInteger.getInternalBigDecimal());
 		subtract.setScale(0);
 		result[1] = new BigInteger(subtract.toString());
-		
+
 		return result;
 	}
 
 	public BigInteger remainder(BigInteger val) {
 		return divideAndRemainder(val)[1];
 	}
-	
+
 //	public BigInteger pow(int exponent) {
 //		InternalBigDecimal i = internalBigInteger.internalBigDecimal.pow(exponent);
 //		InternalBigDecimal i2 = i.setScale(0);
 //		return new BigInteger(i2.toString());
 //	}
 
-	public BigInteger gcd(BigInteger val) {
-		throw new IllegalArgumentException("Method gcd(BigInteger val) not implemented");
-	}
+//	public BigInteger gcd(BigInteger val) {
+//		throw new IllegalArgumentException("Method gcd(BigInteger val) not implemented");
+//	}
 
 	public BigInteger abs() {
 		BigDecimal val = internalBigInteger.abs();
@@ -120,13 +120,13 @@ public class BigInteger extends Number implements Cloneable,Serializable {
 
 	// Modular Arithmetic Operations
 
-	public BigInteger mod(BigInteger m) {
-		throw new IllegalArgumentException("Method mod(BigInteger m) not implemented");
-	}
-
-	public BigInteger modPow(BigInteger exponent, BigInteger m) {
-		throw new IllegalArgumentException("Method modPow(BigInteger exponent, BigInteger m) not implemented");
-	}
+//	public BigInteger mod(BigInteger m) {
+//		throw new IllegalArgumentException("Method mod(BigInteger m) not implemented");
+//	}
+//
+//	public BigInteger modPow(BigInteger exponent, BigInteger m) {
+//		throw new IllegalArgumentException("Method modPow(BigInteger exponent, BigInteger m) not implemented");
+//	}
 
 	public int[] clone(int a[]) {
 		int temp[] = new int[a.length];
@@ -136,72 +136,72 @@ public class BigInteger extends Number implements Cloneable,Serializable {
 		return temp;
 	}
 
-	public BigInteger modInverse(BigInteger m) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
+//	public BigInteger modInverse(BigInteger m) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
 
 	// Shift Operations
 
-	public BigInteger shiftLeft(int n) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
+//	public BigInteger shiftLeft(int n) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public BigInteger shiftRight(int n) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public BigInteger and(BigInteger val) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public BigInteger or(BigInteger val) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public BigInteger xor(BigInteger val) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public BigInteger not() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public BigInteger andNot(BigInteger val) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	// Single Bit Operations
+//
+//	public boolean testBit(int n) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public BigInteger setBit(int n) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public BigInteger clearBit(int n) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public BigInteger flipBit(int n) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public int getLowestSetBit() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	// Miscellaneous Bit Operations
+//
+//	public int bitLength() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public int bitCount() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
 
-	public BigInteger shiftRight(int n) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public BigInteger and(BigInteger val) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public BigInteger or(BigInteger val) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public BigInteger xor(BigInteger val) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public BigInteger not() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public BigInteger andNot(BigInteger val) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	// Single Bit Operations
-
-	public boolean testBit(int n) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public BigInteger setBit(int n) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public BigInteger clearBit(int n) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public BigInteger flipBit(int n) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public int getLowestSetBit() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	// Miscellaneous Bit Operations
-
-	public int bitLength() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public int bitCount() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-	
 	public int compareTo(BigInteger val) {
 		return internalBigInteger.compareTo(val.internalBigInteger);
 	}
@@ -210,10 +210,10 @@ public class BigInteger extends Number implements Cloneable,Serializable {
 		if (!(o instanceof BigInteger)) {
 			throw new IllegalArgumentException("method compareTo only with BigInteger");
 		}
-		
+
 		return compareTo((BigInteger)o);
 	}
-	
+
 	public boolean equals(Object obj) {
 		if(obj == null){
         	return false;
@@ -242,40 +242,40 @@ public class BigInteger extends Number implements Cloneable,Serializable {
 		return internalBigInteger.hashCode();
 	}
 
-	public String toString(int radix) {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
+//	public String toString(int radix) {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
 
-   
+
 	public String toString() {
 		return internalBigInteger.toString();
 	}
 
-	public byte[] toByteArray() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
+//	public byte[] toByteArray() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
 
 	public int intValue() {
 		return new Integer(internalBigInteger.toString()).intValue();
 	}
-	
-	public long longValue() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
 
-	public float floatValue() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public double doubleValue() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public byte byteValue() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
-
-	public short shortValue() {
-		throw new IllegalArgumentException("Method  not implemented");
-	}
+//	public long longValue() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public float floatValue() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public double doubleValue() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public byte byteValue() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
+//
+//	public short shortValue() {
+//		throw new IllegalArgumentException("Method  not implemented");
+//	}
 }
