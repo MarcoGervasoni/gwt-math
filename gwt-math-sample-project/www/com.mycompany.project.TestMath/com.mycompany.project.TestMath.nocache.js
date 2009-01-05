6aa5fe26fba84c4add73deaddda994b5d86fe296
marco.gervasoni <marco.gervasoni@e5a20a11-9342-0410-9350-2f45a7d0be4e>
2009-01-05 23:37:48 +0000

    demo ver 2.0.3

--- gwt-math-sample-project/www/com.mycompany.project.TestMath/com.mycompany.project.TestMath.nocache.js
+++ gwt-math-sample-project/www/com.mycompany.project.TestMath/com.mycompany.project.TestMath.nocache.js
@@ -1,13 +1,317 @@
-function com_mycompany_project_TestMath(){var l=window,k=document,t=l.external,ab,w,q,p='',z={},db=[],F=[],o=[],C,E;if(!l.__gwt_stylesLoaded){l.__gwt_stylesLoaded={};}if(!l.__gwt_scriptsLoaded){l.__gwt_scriptsLoaded={};}function v(){try{return t&&(t.gwtOnLoad&&l.location.search.indexOf('gwt.hybrid')== -1);}catch(a){return false;}}
-function y(){if(ab&&w){var c=k.getElementById('com.mycompany.project.TestMath');var b=c.contentWindow;b.__gwt_initHandlers=com_mycompany_project_TestMath.__gwt_initHandlers;if(v()){b.__gwt_getProperty=function(a){return r(a);};}com_mycompany_project_TestMath=null;b.gwtOnLoad(C,'com.mycompany.project.TestMath',p);}}
-function s(){var j,h='__gwt_marker_com.mycompany.project.TestMath',i;k.write('<script id="'+h+'"><\/script>');i=k.getElementById(h);j=i&&i.previousSibling;while(j&&j.tagName!='SCRIPT'){j=j.previousSibling;}function d(b){var a=b.lastIndexOf('/');return a>=0?b.substring(0,a+1):'';}
-;if(j&&j.src){p=d(j.src);}if(p==''){var c=k.getElementsByTagName('base');if(c.length>0){p=c[c.length-1].href;}else{var g=k.location;var e=g.href;p=d(e.substr(0,e.length-g.hash.length));}}else if(p.match(/^\w+:\/\//)){}else{var f=k.createElement('img');f.src=p+'clear.cache.gif';p=d(f.src);}if(i){i.parentNode.removeChild(i);}}
-function D(){var f=document.getElementsByTagName('meta');for(var d=0,g=f.length;d<g;++d){var e=f[d],h=e.getAttribute('name'),b;if(h){if(h=='gwt:property'){b=e.getAttribute('content');if(b){var i,c=b.indexOf('=');if(c>=0){h=b.substring(0,c);i=b.substring(c+1);}else{h=b;i='';}z[h]=i;}}else if(h=='gwt:onPropertyErrorFn'){b=e.getAttribute('content');if(b){try{E=eval(b);}catch(a){alert('Bad handler "'+b+'" for "gwt:onPropertyErrorFn"');}}}else if(h=='gwt:onLoadErrorFn'){b=e.getAttribute('content');if(b){try{C=eval(b);}catch(a){alert('Bad handler "'+b+'" for "gwt:onLoadErrorFn"');}}}}}}
-function n(a,b){return b in db[a];}
-function m(a){var b=z[a];return b==null?null:b;}
-function cb(d,e){var a=o;for(var b=0,c=d.length-1;b<c;++b){a=a[d[b]]||(a[d[b]]=[]);}a[d[c]]=e;}
-function r(d){var e=F[d](),b=db[d];if(e in b){return e;}var a=[];for(var c in b){a[b[c]]=c;}if(E){E(d,a,e);}throw null;}
-F['user.agent']=function(){var d=navigator.userAgent.toLowerCase();var b=function(a){return parseInt(a[1])*1000+parseInt(a[2]);};if(d.indexOf('opera')!= -1){return 'opera';}else if(d.indexOf('webkit')!= -1){return 'safari';}else if(d.indexOf('msie')!= -1){var c=/msie ([0-9]+)\.([0-9]+)/.exec(d);if(c&&c.length==3){if(b(c)>=6000){return 'ie6';}}}else if(d.indexOf('gecko')!= -1){var c=/rv:([0-9]+)\.([0-9]+)/.exec(d);if(c&&c.length==3){if(b(c)>=1008)return 'gecko1_8';}return 'gecko';}return 'unknown';};db['user.agent']={'gecko':0,'gecko1_8':1,'ie6':2,'opera':3,'safari':4};com_mycompany_project_TestMath.onInjectionDone=function(){ab=true;y();};com_mycompany_project_TestMath.onScriptLoad=function(){w=true;y();};s();D();var bb;if(v()){bb='hosted.html?com_mycompany_project_TestMath';}else{try{cb(['gecko'],'2169C2F8764C6C7B74C11CA41D540872');cb(['safari'],'688D120DA5BC2D2594F44F0B5BB06F76');cb(['gecko1_8'],'7316E0203103E55A92636C776EF5B81E');cb(['opera'],'9EA5037FE473AB5C164C081FDDAA8F27');cb(['ie6'],'E07450FB7C19648C95ECD8B274EA2971');bb=o[r('user.agent')];}catch(a){return;}bb+='.cache.html';}var B;function A(){if(!q){q=true;y();if(k.removeEventListener){k.removeEventListener('DOMContentLoaded',A,false);}if(B){clearInterval(B);}}}
-var u;function x(){if(!u){u=true;var a=k.createElement('iframe');a.src="javascript:''";a.id='com.mycompany.project.TestMath';a.style.cssText='position:absolute;width:0;height:0;border:none';k.body.appendChild(a);a.src=p+bb;}}
-if(k.addEventListener){k.addEventListener('DOMContentLoaded',function(){x();A();},false);}var B=setInterval(function(){if(/loaded|complete/.test(k.readyState)){x();A();}},50);if(!__gwt_scriptsLoaded['js/mathcontext.js']){__gwt_scriptsLoaded['js/mathcontext.js']=true;document.write('<script language="javascript" src="'+p+'js/mathcontext.js"><\/script>');}if(!__gwt_scriptsLoaded['js/bigdecimal.js']){__gwt_scriptsLoaded['js/bigdecimal.js']=true;document.write('<script language="javascript" src="'+p+'js/bigdecimal.js"><\/script>');}k.write("<script>com_mycompany_project_TestMath.onInjectionDone('com.mycompany.project.TestMath')<\/script>");}
-com_mycompany_project_TestMath.__gwt_initHandlers=function(i,e,j){var d=window,g=d.onresize,f=d.onbeforeunload,h=d.onunload;d.onresize=function(a){try{i();}finally{g&&g(a);}};d.onbeforeunload=function(a){var c,b;try{c=e();}finally{b=f&&f(a);}if(c!=null){return c;}if(b!=null){return b;}};d.onunload=function(a){try{j();}finally{h&&h(a);}};};com_mycompany_project_TestMath();
+function com_mycompany_project_TestMath(){
+  var $intern_0 = '', $intern_27 = '" for "gwt:onLoadErrorFn"', $intern_25 = '" for "gwt:onPropertyErrorFn"', $intern_10 = '"><\/script>', $intern_12 = '#', $intern_14 = '/', $intern_45 = '3B98B301557FFF4135664C9F56E5ED00.cache.html', $intern_46 = '7C1FF98B6BD393A4DAA4902B6119A1F6.cache.html', $intern_55 = '<script defer="defer">com_mycompany_project_TestMath.onInjectionDone(\'com.mycompany.project.TestMath\')<\/script>', $intern_9 = '<script id="', $intern_51 = '<script language="javascript" src="', $intern_22 = '=', $intern_13 = '?', $intern_47 = 'BDDF31EA3313B025EFC1E598F4C9167F.cache.html', $intern_24 = 'Bad handler "', $intern_44 = 'CE0E17AFF5899BC947CBA215829796DA.cache.html', $intern_49 = 'DOMContentLoaded', $intern_48 = 'F4E559AF80C7A55520143D744BCAE7FE.cache.html', $intern_11 = 'SCRIPT', $intern_8 = '__gwt_marker_com.mycompany.project.TestMath', $intern_15 = 'base', $intern_4 = 'begin', $intern_3 = 'bootstrap', $intern_17 = 'clear.cache.gif', $intern_1 = 'com.mycompany.project.TestMath', $intern_21 = 'content', $intern_7 = 'end', $intern_38 = 'gecko', $intern_39 = 'gecko1_8', $intern_5 = 'gwt.hybrid', $intern_26 = 'gwt:onLoadErrorFn', $intern_23 = 'gwt:onPropertyErrorFn', $intern_20 = 'gwt:property', $intern_43 = 'hosted.html?com_mycompany_project_TestMath', $intern_37 = 'ie6', $intern_28 = 'iframe', $intern_16 = 'img', $intern_29 = "javascript:''", $intern_50 = 'js/amathcontext.js', $intern_52 = 'js/amathcontext.js"><\/script>', $intern_53 = 'js/bigdecimal.js', $intern_54 = 'js/bigdecimal.js"><\/script>', $intern_41 = 'loadExternalRefs', $intern_18 = 'meta', $intern_31 = 'moduleRequested', $intern_6 = 'moduleStartup', $intern_36 = 'msie', $intern_19 = 'name', $intern_33 = 'opera', $intern_30 = 'position:absolute;width:0;height:0;border:none', $intern_35 = 'safari', $intern_42 = 'selectingPermutation', $intern_2 = 'startup', $intern_40 = 'unknown', $intern_32 = 'user.agent', $intern_34 = 'webkit';
+  var $wnd = window, $doc = document, $stats = $wnd.__gwtStatsEvent?function(a){
+    return $wnd.__gwtStatsEvent(a);
+  }
+  :null, scriptsDone, loadDone, bodyDone, base = $intern_0, metaProps = {}, values = [], providers = [], answers = [], onLoadErrorFunc, propertyErrorFunc;
+  $stats && $stats({moduleName:$intern_1, subSystem:$intern_2, evtGroup:$intern_3, millis:(new Date()).getTime(), type:$intern_4});
+  if (!$wnd.__gwt_stylesLoaded) {
+    $wnd.__gwt_stylesLoaded = {};
+  }
+  if (!$wnd.__gwt_scriptsLoaded) {
+    $wnd.__gwt_scriptsLoaded = {};
+  }
+  function isHostedMode(){
+    try {
+      return $wnd.external && ($wnd.external.gwtOnLoad && $wnd.location.search.indexOf($intern_5) == -1);
+    }
+     catch (e) {
+      return false;
+    }
+  }
+
+  function maybeStartModule(){
+    if (scriptsDone && loadDone) {
+      var iframe = $doc.getElementById($intern_1);
+      var frameWnd = iframe.contentWindow;
+      frameWnd.__gwt_initHandlers = com_mycompany_project_TestMath.__gwt_initHandlers;
+      if (isHostedMode()) {
+        frameWnd.__gwt_getProperty = function(name){
+          return computePropValue(name);
+        }
+        ;
+      }
+      com_mycompany_project_TestMath = null;
+      frameWnd.gwtOnLoad(onLoadErrorFunc, $intern_1, base);
+      $stats && $stats({moduleName:$intern_1, subSystem:$intern_2, evtGroup:$intern_6, millis:(new Date()).getTime(), type:$intern_7});
+    }
+  }
+
+  function computeScriptBase(){
+    var thisScript, markerId = $intern_8, markerScript;
+    $doc.write($intern_9 + markerId + $intern_10);
+    markerScript = $doc.getElementById(markerId);
+    thisScript = markerScript && markerScript.previousSibling;
+    while (thisScript && thisScript.tagName != $intern_11) {
+      thisScript = thisScript.previousSibling;
+    }
+    function getDirectoryOfFile(path){
+      var hashIndex = path.lastIndexOf($intern_12);
+      if (hashIndex == -1) {
+        hashIndex = path.length;
+      }
+      var queryIndex = path.indexOf($intern_13);
+      if (queryIndex == -1) {
+        queryIndex = path.length;
+      }
+      var slashIndex = path.lastIndexOf($intern_14, Math.min(queryIndex, hashIndex));
+      return slashIndex >= 0?path.substring(0, slashIndex + 1):$intern_0;
+    }
+
+    ;
+    if (thisScript && thisScript.src) {
+      base = getDirectoryOfFile(thisScript.src);
+    }
+    if (base == $intern_0) {
+      var baseElements = $doc.getElementsByTagName($intern_15);
+      if (baseElements.length > 0) {
+        base = baseElements[baseElements.length - 1].href;
+      }
+       else {
+        base = getDirectoryOfFile($doc.location.href);
+      }
+    }
+     else if (base.match(/^\w+:\/\//)) {
+    }
+     else {
+      var img = $doc.createElement($intern_16);
+      img.src = base + $intern_17;
+      base = getDirectoryOfFile(img.src);
+    }
+    if (markerScript) {
+      markerScript.parentNode.removeChild(markerScript);
+    }
+  }
+
+  function processMetas(){
+    var metas = document.getElementsByTagName($intern_18);
+    for (var i = 0, n = metas.length; i < n; ++i) {
+      var meta = metas[i], name = meta.getAttribute($intern_19), content;
+      if (name) {
+        if (name == $intern_20) {
+          content = meta.getAttribute($intern_21);
+          if (content) {
+            var value, eq = content.indexOf($intern_22);
+            if (eq >= 0) {
+              name = content.substring(0, eq);
+              value = content.substring(eq + 1);
+            }
+             else {
+              name = content;
+              value = $intern_0;
+            }
+            metaProps[name] = value;
+          }
+        }
+         else if (name == $intern_23) {
+          content = meta.getAttribute($intern_21);
+          if (content) {
+            try {
+              propertyErrorFunc = eval(content);
+            }
+             catch (e) {
+              alert($intern_24 + content + $intern_25);
+            }
+          }
+        }
+         else if (name == $intern_26) {
+          content = meta.getAttribute($intern_21);
+          if (content) {
+            try {
+              onLoadErrorFunc = eval(content);
+            }
+             catch (e) {
+              alert($intern_24 + content + $intern_27);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  function unflattenKeylistIntoAnswers(propValArray, value){
+    var answer = answers;
+    for (var i = 0, n = propValArray.length - 1; i < n; ++i) {
+      answer = answer[propValArray[i]] || (answer[propValArray[i]] = []);
+    }
+    answer[propValArray[n]] = value;
+  }
+
+  function computePropValue(propName){
+    var value = providers[propName](), allowedValuesMap = values[propName];
+    if (value in allowedValuesMap) {
+      return value;
+    }
+    var allowedValuesList = [];
+    for (var k in allowedValuesMap) {
+      allowedValuesList[allowedValuesMap[k]] = k;
+    }
+    if (propertyErrorFunc) {
+      propertyErrorFunc(propName, allowedValuesList, value);
+    }
+    throw null;
+  }
+
+  var frameInjected;
+  function maybeInjectFrame(){
+    if (!frameInjected) {
+      frameInjected = true;
+      var iframe = $doc.createElement($intern_28);
+      iframe.src = $intern_29;
+      iframe.id = $intern_1;
+      iframe.style.cssText = $intern_30;
+      iframe.tabIndex = -1;
+      $doc.body.appendChild(iframe);
+      $stats && $stats({moduleName:$intern_1, subSystem:$intern_2, evtGroup:$intern_6, millis:(new Date()).getTime(), type:$intern_31});
+      iframe.contentWindow.location.replace(base + strongName);
+    }
+  }
+
+  providers[$intern_32] = function(){
+    var ua = navigator.userAgent.toLowerCase();
+    var makeVersion = function(result){
+      return parseInt(result[1]) * 1000 + parseInt(result[2]);
+    }
+    ;
+    if (ua.indexOf($intern_33) != -1) {
+      return $intern_33;
+    }
+     else if (ua.indexOf($intern_34) != -1) {
+      return $intern_35;
+    }
+     else if (ua.indexOf($intern_36) != -1) {
+      var result = /msie ([0-9]+)\.([0-9]+)/.exec(ua);
+      if (result && result.length == 3) {
+        if (makeVersion(result) >= 6000) {
+          return $intern_37;
+        }
+      }
+    }
+     else if (ua.indexOf($intern_38) != -1) {
+      var result = /rv:([0-9]+)\.([0-9]+)/.exec(ua);
+      if (result && result.length == 3) {
+        if (makeVersion(result) >= 1008)
+          return $intern_39;
+      }
+      return $intern_38;
+    }
+    return $intern_40;
+  }
+  ;
+  values[$intern_32] = {gecko:0, gecko1_8:1, ie6:2, opera:3, safari:4};
+  com_mycompany_project_TestMath.onScriptLoad = function(){
+    if (frameInjected) {
+      loadDone = true;
+      maybeStartModule();
+    }
+  }
+  ;
+  com_mycompany_project_TestMath.onInjectionDone = function(){
+    scriptsDone = true;
+    $stats && $stats({moduleName:$intern_1, subSystem:$intern_2, evtGroup:$intern_41, millis:(new Date()).getTime(), type:$intern_7});
+    maybeStartModule();
+  }
+  ;
+  computeScriptBase();
+  processMetas();
+  $stats && $stats({moduleName:$intern_1, subSystem:$intern_2, evtGroup:$intern_3, millis:(new Date()).getTime(), type:$intern_42});
+  var strongName;
+  if (isHostedMode()) {
+    strongName = $intern_43;
+  }
+   else {
+    try {
+      unflattenKeylistIntoAnswers([$intern_33], $intern_44);
+      unflattenKeylistIntoAnswers([$intern_35], $intern_45);
+      unflattenKeylistIntoAnswers([$intern_39], $intern_46);
+      unflattenKeylistIntoAnswers([$intern_38], $intern_47);
+      unflattenKeylistIntoAnswers([$intern_37], $intern_48);
+      strongName = answers[computePropValue($intern_32)];
+    }
+     catch (e) {
+      return;
+    }
+  }
+  var onBodyDoneTimerId;
+  function onBodyDone(){
+    if (!bodyDone) {
+      bodyDone = true;
+      maybeStartModule();
+      if ($doc.removeEventListener) {
+        $doc.removeEventListener($intern_49, onBodyDone, false);
+      }
+      if (onBodyDoneTimerId) {
+        clearInterval(onBodyDoneTimerId);
+      }
+    }
+  }
+
+  if ($doc.addEventListener) {
+    $doc.addEventListener($intern_49, function(){
+      maybeInjectFrame();
+      onBodyDone();
+    }
+    , false);
+  }
+  var onBodyDoneTimerId = setInterval(function(){
+    if (/loaded|complete/.test($doc.readyState)) {
+      maybeInjectFrame();
+      onBodyDone();
+    }
+  }
+  , 50);
+  $stats && $stats({moduleName:$intern_1, subSystem:$intern_2, evtGroup:$intern_3, millis:(new Date()).getTime(), type:$intern_7});
+  $stats && $stats({moduleName:$intern_1, subSystem:$intern_2, evtGroup:$intern_41, millis:(new Date()).getTime(), type:$intern_4});
+  if (!__gwt_scriptsLoaded[$intern_50]) {
+    __gwt_scriptsLoaded[$intern_50] = true;
+    document.write($intern_51 + base + $intern_52);
+  }
+  if (!__gwt_scriptsLoaded[$intern_53]) {
+    __gwt_scriptsLoaded[$intern_53] = true;
+    document.write($intern_51 + base + $intern_54);
+  }
+  $doc.write($intern_55);
+}
+
+com_mycompany_project_TestMath.__gwt_initHandlers = function(resize, beforeunload, unload){
+  var $wnd = window, oldOnResize = $wnd.onresize, oldOnBeforeUnload = $wnd.onbeforeunload, oldOnUnload = $wnd.onunload;
+  $wnd.onresize = function(evt){
+    try {
+      resize();
+    }
+     finally {
+      oldOnResize && oldOnResize(evt);
+    }
+  }
+  ;
+  $wnd.onbeforeunload = function(evt){
+    var ret, oldRet;
+    try {
+      ret = beforeunload();
+    }
+     finally {
+      oldRet = oldOnBeforeUnload && oldOnBeforeUnload(evt);
+    }
+    if (ret != null) {
+      return ret;
+    }
+    if (oldRet != null) {
+      return oldRet;
+    }
+  }
+  ;
+  $wnd.onunload = function(evt){
+    try {
+      unload();
+    }
+     finally {
+      oldOnUnload && oldOnUnload(evt);
+      $wnd.onresize = null;
+      $wnd.onbeforeunload = null;
+      $wnd.onunload = null;
+    }
+  }
+  ;
+}
+;
+com_mycompany_project_TestMath();
