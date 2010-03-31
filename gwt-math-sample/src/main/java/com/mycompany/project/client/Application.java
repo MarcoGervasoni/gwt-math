12af58aba493f4b6296cbdba3f92a53c6ba33d90
marco.gervasoni <marco.gervasoni@e5a20a11-9342-0410-9350-2f45a7d0be4e>
2010-03-31 14:55:28 +0000

    progetto di prova mavenized

---
+++ gwt-math-sample/src/main/java/com/mycompany/project/client/Application.java
@@ -0,0 +1,66 @@
+package com.mycompany.project.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MenuItem;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Application implements EntryPoint {
+
+	private SimplePanel page;
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		final RootPanel rootPanel = RootPanel.get();
+		
+		HorizontalPanel hp = new HorizontalPanel();
+		page = new SimplePanel();
+
+		final Label sampleExampleToLabel = new Label(
+				"Sample example to make operation client-side");
+		rootPanel.add(sampleExampleToLabel);
+
+		MenuBar bar = new MenuBar();
+		bar.addItem(new MenuItem("Calculator BigDecimal", new Command() {
+
+			public void execute() {
+				clearAll(new BigDecimalTest());
+			}
+		}));
+		bar.addItem(new MenuItem("Calculator BigInteger", new Command() {
+
+			public void execute() {
+				clearAll(new BigIntegerTest());
+			}
+		}));
+		bar.addItem(new MenuItem("TestCase BigDecimal", new Command() {
+
+			public void execute() {
+				clearAll(new TestCaseBigDecimal());
+			}
+		}));
+		
+		hp.add(bar);
+		rootPanel.add(hp);
+		
+		rootPanel.add(page);
+
+	}
+
+	public void clearAll(Widget p) {
+		RootPanel.get().remove(page);
+		page = new SimplePanel();
+		page.add(p);
+		RootPanel.get().add(page);
+	}
+}
